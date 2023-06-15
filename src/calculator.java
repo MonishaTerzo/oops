852b80b8fbf36f24a25a0de58f197b71d909bd9e
MonishaTerzo <monisha@terzocloud.com>
2023-06-15 10:53:33 +0530

    Atm_project

---
+++ src/calculator.java
@@ -0,0 +1,21 @@
+public class calculator {
+    public double add(double a, double b) {
+        return a + b;
+    }
+
+    public double subtract(double a, double b) {
+        return a - b;
+    }
+
+    public double multiply(double a, double b) {
+        return a * b;
+    }
+
+ public double divide(double a, double b) {
+        if (b != 0) {
+            return a / b;
+        } else {
+            throw new ArithmeticException("Cannot divide by zero because zero gives infinity....so please dont provide zero");
+        }
+    }
+}
